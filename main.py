893a375174519e937caa61022ac78894e26c0488
angelicacabato <58801573+angelicacabato@users.noreply.github.com>
2024-03-25 20:48:49 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+GROUP 1: Anunay Amrit, Angelica Cabato, Pranav Vijay Chand, Riya Chapatwala, Sai Satya Jagannadh Doddipatla, Nhat Ho
+
+Dr. Shah
+
+CPSC 535: Advanced Algorithms (Spring 2024)
+
+"""
+def print_hi(name):
+   
+    print(f'Hi, {name}')
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
